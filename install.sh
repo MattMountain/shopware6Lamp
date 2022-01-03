736ccdb31642f23235710c53f9ec09e1b1641e70
Matthias Riedl <matthiasriedl@Matthiass-Mac-mini.fritz.box>
2022-01-03 14:31:57 +0100

    add changes

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/shopware/development.git shopware
+rm -R ./shopware/platform && git clone https://github.com/shopware/platform.git platform
 chmod -Rf 777 shopware 
 cp ./data/shopware/.env /shopware/.env
